27d5859755ed364e2054f0006efa646cea6935d1
Rafael Gonzaga <rafaelgonzagacontato@gmail.com>
2022-01-11 16:47:21 -0300

    Requisito 3 ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const entrar = document.getElementById('bt-login');
+
+function verificaLogin() {
+    const email = document.getElementById('email');
+    const senha = document.getElementById('password');
+    if(email.value === '' || senha.value === ''){
+        return alert('Email ou senha inválidos.');
+    }
+    return alert('Olá, Tryber!');
+}
+entrar.addEventListener('click', verificaLogin);
